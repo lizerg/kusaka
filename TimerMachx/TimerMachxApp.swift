565d85973007d2c85ebc183632ff8dc335e8cc2b
Kikimora32 <82947993+Kikimora32@users.noreply.github.com>
2022-04-13 09:38:13 +0300

    Initial Commit

---
+++ TimerMachx/TimerMachxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerMachxApp.swift
+//  TimerMachx
+//
+//  Created by Student on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerMachxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
